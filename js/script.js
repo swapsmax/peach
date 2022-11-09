59890f9227b126855d318657f2f635244115ec57
Swapnil <46750394+swapsmax@users.noreply.github.com>
2022-11-09 21:48:00 +0800

    Added sub-page for skin treatment

--- js/script.js
+++ js/script.js
@@ -1420,6 +1420,27 @@
 		}
 
 
+		/**
+		 * Vertical tabs for services page
+		 */
+		 $(document).on("click", ".naccs .menu div", function() {
+			var numberIndex = $(this).index();
+		
+			if (!$(this).is("active")) {
+				$(".naccs .menu div").removeClass("active");
+				$(".naccs ul li").removeClass("active");
+		
+				$(this).addClass("active");
+				$(".naccs ul").find("li:eq(" + numberIndex + ")").addClass("active");
+		
+				var listItemHeight = $(".naccs ul")
+					.find("li:eq(" + numberIndex + ")")
+					.innerHeight();
+				$(".naccs ul").height(listItemHeight + "px");
+			}
+		});
+
+
 
 
 	});
